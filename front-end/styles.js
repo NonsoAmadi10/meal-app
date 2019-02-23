71318e15575e0bd8bd892cf1af5277f414c37498
Nonso Amadi <nonsoamadi@aol.com>
2019-02-23 14:05:59 +0100

    bootstapping application

---
+++ front-end/styles.js
@@ -0,0 +1,28 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+
+
+  //Cart modal
+  // Get the modal
+let modal = document.getElementById('id01');
+let modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
